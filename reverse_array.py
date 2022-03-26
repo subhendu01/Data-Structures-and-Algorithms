b3a5aa738025ec51affc1d205839c891c63c8c1c
Subhendu Sekhar Baug <subhendu.baug@gmail.com>
2022-03-26 22:56:52 +0530

    reverse array file added

--- reverse_array.py
+++ reverse_array.py
@@ -9,17 +9,21 @@ def reverse_iterative(lst):
         lst[l-i-1] = n
     return lst
 
+#using recursive method
 def reverse_recursive_sol(lst):
     if len(lst) == 1:
         return lst
     return reverse_recursive_sol(lst[1:]) + lst[0:1]
 
+#using slicing
 def reverse_(lst):
     return lst[::-1]
 
+#using reversed method
 def _reverse_(lst):
     return [i for i in reversed(lst)]
 
+#using reverse method
 def reverse_1(lst):
     lst.reverse()
     return lst
